975325247aee2247dbb7026b1012ba22e7c01905
Thomas Sowell <tom@ldtlb.com>
2025-01-09 18:11:13 -0900

    Track nodes being monitored

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,12 @@
+use std::collections::HashMap;
+use std::rc::Rc;
+use std::cell::RefCell;
+
 use pipewire::{
     main_loop::MainLoop,
     context::Context,
     registry::GlobalObject,
+    properties::Properties,
     types::ObjectType
 };
 use libspa::utils::dict::DictRef;
@@ -13,23 +18,33 @@ const MEDIA_CLASSES: &[&str] = &[
     "Stream/Output/Audio",
 ];
 
-fn global(global: &GlobalObject<&DictRef>) {
-    match global.type_ {
-        ObjectType::Node => {
-            if let Some(props) = &global.props {
-                if let Some(media_class) = props.get("media.class") {
-                    if MEDIA_CLASSES.contains(&media_class) {
-                        println!("{}: {:?}", media_class, global);
-                    }
-                }
-            }
-        },
-        _ => (),
-    }
+#[derive(Default)]
+struct GlobalListener {
+    nodes: HashMap<u32, GlobalObject<Properties>>,
 }
 
-fn global_remove(global_remove: u32) {
-    println!("Remove: {}", global_remove);
+impl GlobalListener {
+    fn global(&mut self, global: &GlobalObject<&DictRef>) {
+        match global.type_ {
+            ObjectType::Node => {
+                if let Some(props) = &global.props {
+                    if let Some(media_class) = props.get("media.class") {
+                        if MEDIA_CLASSES.contains(&media_class) {
+                            println!("{}: {:?}", media_class, global);
+                            self.nodes.insert(global.id, global.to_owned());
+                        }
+                    }
+                }
+            },
+            _ => (),
+        }
+    }
+
+    fn global_remove(&mut self, id: u32) {
+        if self.nodes.remove(&id).is_some() {
+            println!("Removed: {}", id);
+        }
+    }
 }
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
@@ -37,11 +52,17 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let context = Context::new(&mainloop)?;
     let core = context.connect(None)?;
     let registry = core.get_registry()?;
+    let global_listener = Rc::new(RefCell::new(GlobalListener::default()));
+    let global_remove_listener = Rc::clone(&global_listener);
 
     let _listener = registry
         .add_listener_local()
-        .global(global)
-        .global_remove(global_remove)
+        .global(move |global| {
+            global_listener.borrow_mut().global(global)
+        })
+        .global_remove(move |id| {
+            global_remove_listener.borrow_mut().global_remove(id)
+        })
         .register();
 
     mainloop.run();
