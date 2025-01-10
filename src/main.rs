8be5e34ea8295094a22488432f604b7ae65136a6
Thomas Sowell <tom@ldtlb.com>
2025-01-09 19:02:07 -0900

    Bind global objects to Nodes

--- src/main.rs
+++ src/main.rs
@@ -5,11 +5,11 @@ use std::cell::RefCell;
 use pipewire::{
     main_loop::MainLoop,
     context::Context,
-    registry::GlobalObject,
-    properties::Properties,
+    registry::Listener,
+    core::Core,
+    node::Node,
     types::ObjectType
 };
-use libspa::utils::dict::DictRef;
 
 const MEDIA_CLASSES: &[&str] = &[
     "Audio/Device",
@@ -18,54 +18,67 @@ const MEDIA_CLASSES: &[&str] = &[
     "Stream/Output/Audio",
 ];
 
-#[derive(Default)]
-struct GlobalListener {
-    nodes: HashMap<u32, GlobalObject<Properties>>,
+struct PipewireListener {
+    mainloop: MainLoop,
+    _context: Context,
+    _core: Core,
+    _listener: Listener,
 }
 
-impl GlobalListener {
-    fn global(&mut self, global: &GlobalObject<&DictRef>) {
-        match global.type_ {
-            ObjectType::Node => {
-                if let Some(props) = &global.props {
-                    if let Some(media_class) = props.get("media.class") {
-                        if MEDIA_CLASSES.contains(&media_class) {
-                            println!("{}: {:?}", media_class, global);
-                            self.nodes.insert(global.id, global.to_owned());
+impl PipewireListener {
+    pub fn try_new() -> Result<Self, Box<dyn std::error::Error>> {
+        let mainloop = MainLoop::new(None)?;
+        let context = Context::new(&mainloop)?;
+        let core = context.connect(None)?;
+        let registry = Rc::new(RefCell::new(core.get_registry()?));
+        let nodes = Rc::new(RefCell::new(HashMap::<u32, Node>::new()));
+
+        let nodes_remove = Rc::clone(&nodes);
+        let registry_bind = Rc::clone(&registry);
+
+        let listener = registry.borrow()
+            .add_listener_local()
+            .global(move |global| {
+                match global.type_ {
+                    ObjectType::Node => {
+                        if let Some(props) = &global.props {
+                            if let Some(media_class) = props.get("media.class") {
+                                if MEDIA_CLASSES.contains(&media_class) {
+                                    println!("{}: {:?}", media_class, global);
+                                    if let Ok(node) = registry_bind.borrow().bind(global) {
+                                        nodes.borrow_mut().insert(global.id, node);
+                                    }
+                                }
+                            }
                         }
-                    }
+                    },
+                    _ => (),
                 }
-            },
-            _ => (),
-        }
+            })
+            .global_remove(move |id| {
+                if nodes_remove.borrow_mut().remove(&id).is_some() {
+                    println!("Removed: {}", id);
+                }
+            })
+            .register();
+
+        Ok(Self {
+            mainloop,
+            _context: context,
+            _core: core,
+            _listener: listener,
+        })
     }
 
-    fn global_remove(&mut self, id: u32) {
-        if self.nodes.remove(&id).is_some() {
-            println!("Removed: {}", id);
-        }
+    pub fn run(&self) {
+        self.mainloop.run()
     }
 }
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let mainloop = MainLoop::new(None)?;
-    let context = Context::new(&mainloop)?;
-    let core = context.connect(None)?;
-    let registry = core.get_registry()?;
-    let global_listener = Rc::new(RefCell::new(GlobalListener::default()));
-    let global_remove_listener = Rc::clone(&global_listener);
+    let listener = PipewireListener::try_new()?;
 
-    let _listener = registry
-        .add_listener_local()
-        .global(move |global| {
-            global_listener.borrow_mut().global(global)
-        })
-        .global_remove(move |id| {
-            global_remove_listener.borrow_mut().global_remove(id)
-        })
-        .register();
-
-    mainloop.run();
+    listener.run();
 
     Ok(())
 }
