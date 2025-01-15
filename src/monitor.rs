e5aac396cd88e2701f9af00dbd429ff75f05e3d9
Thomas Sowell <tom@ldtlb.com>
2025-01-14 17:51:01 -0900

    Clean up stream removal

--- src/monitor.rs
+++ src/monitor.rs
@@ -509,12 +509,9 @@ pub fn monitor_pipewire(
             // - proxies owning a ref on Proxy as well
             let proxies_weak = Rc::downgrade(&proxies);
 
-            let stream_info = match s {
-                Some((ref stream_spe, _)) => {
-                    Some((Rc::downgrade(&streams), Rc::downgrade(&stream_spe)))
-                }
-                None => None,
-            };
+            let stream_info = s.as_ref().map(|(stream_spe, _)| {
+                (Rc::downgrade(&streams), Rc::downgrade(&stream_spe))
+            });
             let sender_weak = Rc::downgrade(&sender);
             let listener = proxy
                 .add_listener_local()
@@ -527,18 +524,24 @@ pub fn monitor_pipewire(
                     };
 
                     proxies.borrow_mut().remove(proxy_id);
-                    let message = MonitorMessage::Removed(proxy_id);
-                    sender.send(message);
 
-                    if let Some((streams_weak, stream_spe_weak)) = &stream_info
-                    {
-                        if let (Some(streams), Some(stream_spe)) =
-                            (streams_weak.upgrade(), stream_spe_weak.upgrade())
-                        {
-                            stream_spe.disconnect();
-                            streams.borrow_mut().remove(proxy_id);
-                        }
-                    }
+                    sender.send(MonitorMessage::Removed(proxy_id));
+                })
+                .removed(move || {
+                    let Some((ref streams_weak, ref stream_spe_weak)) =
+                        stream_info
+                    else {
+                        return;
+                    };
+                    let Some(streams) = streams_weak.upgrade() else {
+                        return;
+                    };
+                    let Some(stream_spe) = stream_spe_weak.upgrade() else {
+                        return;
+                    };
+
+                    let _ = stream_spe.disconnect();
+                    streams.borrow_mut().remove(proxy_id);
                 })
                 .register();
 
