632c64364c483f96ced93edd59c473e8b9b552e5
Thomas Sowell <tom@ldtlb.com>
2025-03-11 19:24:44 -0800

    Move config::Keybinding impl into sub-module

--- src/config.rs
+++ src/config.rs
@@ -1,6 +1,7 @@
 //! Mixer configuration.
 
 mod char_set;
+mod keybinding;
 mod name_template;
 mod names;
 mod tag;
@@ -134,107 +135,6 @@ pub struct CharSet {
     pub tab_selected_left: String,
     /// Surrounds (right) selected tab in tab menu
     pub tab_selected_right: String,
-}
-
-impl Keybinding {
-    fn key_event_from(code: KeyCode) -> KeyEvent {
-        KeyEvent::new(code, KeyModifiers::NONE)
-    }
-
-    fn defaults() -> HashMap<KeyEvent, Action> {
-        HashMap::from([
-            (Self::key_event_from(KeyCode::Char('q')), Action::Exit),
-            (Self::key_event_from(KeyCode::Char('m')), Action::ToggleMute),
-            (Self::key_event_from(KeyCode::Char('d')), Action::SetDefault),
-            (
-                Self::key_event_from(KeyCode::Char('l')),
-                Action::SetRelativeVolume(0.01),
-            ),
-            (
-                Self::key_event_from(KeyCode::Char('h')),
-                Action::SetRelativeVolume(-0.01),
-            ),
-            (
-                Self::key_event_from(KeyCode::Char('c')),
-                Action::OpenDropdown,
-            ),
-            (Self::key_event_from(KeyCode::Esc), Action::CloseDropdown),
-            (Self::key_event_from(KeyCode::Enter), Action::SelectDropdown),
-            (Self::key_event_from(KeyCode::Char('j')), Action::MoveDown),
-            (Self::key_event_from(KeyCode::Char('k')), Action::MoveUp),
-            (Self::key_event_from(KeyCode::Char('H')), Action::TabLeft),
-            (Self::key_event_from(KeyCode::Char('L')), Action::TabRight),
-            (
-                Self::key_event_from(KeyCode::Char('`')),
-                Action::SetAbsoluteVolume(0.00),
-            ),
-            (
-                Self::key_event_from(KeyCode::Char('1')),
-                Action::SetAbsoluteVolume(0.10),
-            ),
-            (
-                Self::key_event_from(KeyCode::Char('2')),
-                Action::SetAbsoluteVolume(0.20),
-            ),
-            (
-                Self::key_event_from(KeyCode::Char('3')),
-                Action::SetAbsoluteVolume(0.30),
-            ),
-            (
-                Self::key_event_from(KeyCode::Char('4')),
-                Action::SetAbsoluteVolume(0.40),
-            ),
-            (
-                Self::key_event_from(KeyCode::Char('5')),
-                Action::SetAbsoluteVolume(0.50),
-            ),
-            (
-                Self::key_event_from(KeyCode::Char('6')),
-                Action::SetAbsoluteVolume(0.60),
-            ),
-            (
-                Self::key_event_from(KeyCode::Char('7')),
-                Action::SetAbsoluteVolume(0.70),
-            ),
-            (
-                Self::key_event_from(KeyCode::Char('8')),
-                Action::SetAbsoluteVolume(0.80),
-            ),
-            (
-                Self::key_event_from(KeyCode::Char('9')),
-                Action::SetAbsoluteVolume(0.90),
-            ),
-            (
-                Self::key_event_from(KeyCode::Char('0')),
-                Action::SetAbsoluteVolume(1.00),
-            ),
-        ])
-    }
-
-    fn default_modifiers() -> KeyModifiers {
-        KeyModifiers::NONE
-    }
-
-    /// Merge deserialized keybindings with defaults
-    fn merge<'de, D>(
-        deserializer: D,
-    ) -> Result<HashMap<KeyEvent, Action>, D::Error>
-    where
-        D: serde::Deserializer<'de>,
-    {
-        let mut keybindings = Self::defaults();
-
-        let configured = Vec::<Keybinding>::deserialize(deserializer)?;
-
-        for keybinding in configured.into_iter() {
-            keybindings.insert(
-                KeyEvent::new(keybinding.key, keybinding.modifiers),
-                keybinding.action,
-            );
-        }
-
-        Ok(keybindings)
-    }
 }
 
 fn default_char_set_name() -> String {
---
+++ src/config/keybinding.rs
@@ -0,0 +1,110 @@
+//! Implementation for [`Keybinding`](`crate::config::Keybinding`). Defines
+//! default bindings and handles merging of configured bindings with defaults.
+
+use std::collections::HashMap;
+
+use crossterm::event::{KeyCode, KeyEvent, KeyModifiers};
+use serde::Deserialize;
+
+use crate::config::{Action, Keybinding};
+
+impl Keybinding {
+    fn key_event_from(code: KeyCode) -> KeyEvent {
+        KeyEvent::new(code, KeyModifiers::NONE)
+    }
+
+    pub fn defaults() -> HashMap<KeyEvent, Action> {
+        HashMap::from([
+            (Self::key_event_from(KeyCode::Char('q')), Action::Exit),
+            (Self::key_event_from(KeyCode::Char('m')), Action::ToggleMute),
+            (Self::key_event_from(KeyCode::Char('d')), Action::SetDefault),
+            (
+                Self::key_event_from(KeyCode::Char('l')),
+                Action::SetRelativeVolume(0.01),
+            ),
+            (
+                Self::key_event_from(KeyCode::Char('h')),
+                Action::SetRelativeVolume(-0.01),
+            ),
+            (
+                Self::key_event_from(KeyCode::Char('c')),
+                Action::OpenDropdown,
+            ),
+            (Self::key_event_from(KeyCode::Esc), Action::CloseDropdown),
+            (Self::key_event_from(KeyCode::Enter), Action::SelectDropdown),
+            (Self::key_event_from(KeyCode::Char('j')), Action::MoveDown),
+            (Self::key_event_from(KeyCode::Char('k')), Action::MoveUp),
+            (Self::key_event_from(KeyCode::Char('H')), Action::TabLeft),
+            (Self::key_event_from(KeyCode::Char('L')), Action::TabRight),
+            (
+                Self::key_event_from(KeyCode::Char('`')),
+                Action::SetAbsoluteVolume(0.00),
+            ),
+            (
+                Self::key_event_from(KeyCode::Char('1')),
+                Action::SetAbsoluteVolume(0.10),
+            ),
+            (
+                Self::key_event_from(KeyCode::Char('2')),
+                Action::SetAbsoluteVolume(0.20),
+            ),
+            (
+                Self::key_event_from(KeyCode::Char('3')),
+                Action::SetAbsoluteVolume(0.30),
+            ),
+            (
+                Self::key_event_from(KeyCode::Char('4')),
+                Action::SetAbsoluteVolume(0.40),
+            ),
+            (
+                Self::key_event_from(KeyCode::Char('5')),
+                Action::SetAbsoluteVolume(0.50),
+            ),
+            (
+                Self::key_event_from(KeyCode::Char('6')),
+                Action::SetAbsoluteVolume(0.60),
+            ),
+            (
+                Self::key_event_from(KeyCode::Char('7')),
+                Action::SetAbsoluteVolume(0.70),
+            ),
+            (
+                Self::key_event_from(KeyCode::Char('8')),
+                Action::SetAbsoluteVolume(0.80),
+            ),
+            (
+                Self::key_event_from(KeyCode::Char('9')),
+                Action::SetAbsoluteVolume(0.90),
+            ),
+            (
+                Self::key_event_from(KeyCode::Char('0')),
+                Action::SetAbsoluteVolume(1.00),
+            ),
+        ])
+    }
+
+    pub fn default_modifiers() -> KeyModifiers {
+        KeyModifiers::NONE
+    }
+
+    /// Merge deserialized keybindings with defaults
+    pub fn merge<'de, D>(
+        deserializer: D,
+    ) -> Result<HashMap<KeyEvent, Action>, D::Error>
+    where
+        D: serde::Deserializer<'de>,
+    {
+        let mut keybindings = Self::defaults();
+
+        let configured = Vec::<Keybinding>::deserialize(deserializer)?;
+
+        for keybinding in configured.into_iter() {
+            keybindings.insert(
+                KeyEvent::new(keybinding.key, keybinding.modifiers),
+                keybinding.action,
+            );
+        }
+
+        Ok(keybindings)
+    }
+}
