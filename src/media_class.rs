5645973091bf948ef4972ac42895d50b2e2b247a
Thomas Sowell <tom@ldtlb.com>
2025-07-03 03:56:57 -0800

    wirehose refactor: Unused media_class methods

--- src/media_class.rs
+++ src/media_class.rs
@@ -15,30 +15,3 @@ pub fn is_sink_input(s: &str) -> bool {
 pub fn is_source_output(s: &str) -> bool {
     s == "Stream/Input/Audio"
 }
-
-pub fn is_monitor(s: &str) -> bool {
-    s == "Audio/Sink"
-}
-
-pub fn is_recordable(s: &str) -> bool {
-    is_source(s) || is_sink(s) || is_sink_input(s)
-}
-
-pub fn is_duplex(s: &str) -> bool {
-    s == "Audio/Duplex"
-}
-
-pub fn is_virtual(s: &str) -> bool {
-    s == "Audio/Source/Virtual"
-}
-
-pub fn is_stream(s: &str) -> bool {
-    is_sink_input(s) || is_source_output(s)
-}
-
-pub fn is_device(s: &str) -> bool {
-    matches!(
-        s,
-        "Audio/Sink" | "Audio/Source" | "Audio/Duplex" | "Audio/Source/Virtual"
-    )
-}
