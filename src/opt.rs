8459b9bdf9b407e565fc59ba16fc83d9443b3c10
Thomas Sowell <tom@ldtlb.com>
2026-01-14 18:01:59 -0900

    Use doc comments for clap help text

--- src/opt.rs
+++ src/opt.rs
@@ -19,92 +19,61 @@ const VERSION: &str = match option_env!("VERGEN_GIT_DESCRIBE") {
 #[clap(name = "wiremix", about = "PipeWire mixer")]
 #[command(version = VERSION)]
 pub struct Opt {
-    #[clap(
-        short = 'c',
-        long,
-        value_name = "FILE",
-        help = "Override default config file path"
-    )]
+    /// Override default config file path
+    #[clap(short = 'c', long, value_name = "FILE")]
     pub config: Option<PathBuf>,
 
-    #[clap(
-        short,
-        long,
-        value_name = "NAME",
-        help = "The name of the remote to connect to"
-    )]
+    /// The name of the remote to connect to
+    #[clap(short, long, value_name = "NAME")]
     pub remote: Option<String>,
 
-    #[clap(
-        short,
-        long,
-        help = "Target frames per second (or 0 for unlimited)"
-    )]
+    /// Target frames per second (or 0 for unlimited)
+    #[clap(short, long)]
     pub fps: Option<f32>,
 
-    #[clap(
-        short = 's',
-        long,
-        value_name = "NAME",
-        help = "Character set to use [built-in sets: default, compat, extracompat]"
-    )]
+    /// Character set to use [built-in sets: default, compat, extracompat]
+    #[clap(short = 's', long, value_name = "NAME")]
     pub char_set: Option<String>,
 
-    #[clap(
-        short,
-        long,
-        value_name = "NAME",
-        help = "Theme to use [built-in themes: default, nocolor, plain]"
-    )]
+    /// Theme to use [built-in themes: default, nocolor, plain]
+    #[clap(short, long, value_name = "NAME")]
     pub theme: Option<String>,
 
-    #[clap(
-        short,
-        long,
-        value_parser = clap::value_parser!(config::Peaks),
-        help = "Audio peak meters"
-    )]
+    /// Audio peak meters
+    #[clap(short, long, value_parser = clap::value_parser!(config::Peaks))]
     pub peaks: Option<config::Peaks>,
 
-    #[clap(long, conflicts_with = "mouse", help = "Disable mouse support")]
+    /// Disable mouse support
+    #[clap(long, conflicts_with = "mouse")]
     pub no_mouse: bool,
 
-    #[clap(long, conflicts_with = "no_mouse", help = "Enable mouse support")]
+    /// Enable mouse support
+    #[clap(long, conflicts_with = "no_mouse")]
     pub mouse: bool,
 
+    /// Initial tab view
     #[clap(
         short = 'v',
         long,
         value_enum,
         value_parser = clap::value_parser!(TabKind),
-        help = "Initial tab view"
     )]
     pub tab: Option<TabKind>,
 
-    #[clap(
-        short = 'm',
-        long,
-        value_name = "PERCENT",
-        help = "Maximum volume for volume sliders"
-    )]
+    /// Maximum volume for volume sliders
+    #[clap(short = 'm', long, value_name = "PERCENT")]
     pub max_volume_percent: Option<f32>,
 
-    #[clap(
-        long,
-        conflicts_with = "enforce_max_volume",
-        help = "Allow increasing volume past max-volume-percent"
-    )]
+    /// Allow increasing volume past max-volume-percent
+    #[clap(long, conflicts_with = "enforce_max_volume")]
     pub no_enforce_max_volume: bool,
 
-    #[clap(
-        long,
-        conflicts_with = "no_enforce_max_volume",
-        help = "Prevent increasing volume past max-volume-percent"
-    )]
+    /// Prevent increasing volume past max-volume-percent
+    #[clap(long, conflicts_with = "no_enforce_max_volume")]
     pub enforce_max_volume: bool,
 
     #[cfg(debug_assertions)]
-    #[clap(short, long, help = "Dump events without showing interface")]
+    #[clap(short, long)]
     pub dump_events: bool,
 }
 
